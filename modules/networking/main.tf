af8b8df5901219a659e31d0756ad99bbc78e79fe
a <a>
2025-02-01 19:30:41 +0000

    specifying availability zones

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -7,12 +7,14 @@ resource "aws_subnet" "first" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 }
 
 resource "aws_subnet" "second" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
 }
 
 resource "aws_internet_gateway" "igw" {
